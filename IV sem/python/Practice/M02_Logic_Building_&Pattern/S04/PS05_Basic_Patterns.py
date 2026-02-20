dcbf3a303bfac9cd425d2c9c590ccbf73bb3e277
revanthkonakalla53-sketch <revanthkonakalla53@gmail.com>
2026-02-20 11:13:54 +0530

    pushing the previous files

--- IV sem/python/Practice/M02_Logic_Building_&Pattern/S04/PS05_Basic_Patterns.py
+++ IV sem/python/Practice/M02_Logic_Building_&Pattern/S04/PS05_Basic_Patterns.py
@@ -1,8 +1,55 @@
-'''
-input :4
-output"
-* * * *
-* * * *
-* * * *
-* * * *
-'''
+# # # # # '''
+# # # # # input :4
+# # # # # output"
+# # # # # * * * *
+# # # # # * * * *
+# # # # # * * * *
+# # # # # * * * *
+# # # # # '''
+# # # # # # n = int(input())
+# # # # # # for i in range(n):
+# # # # # #     for j in range(n):
+# # # # # #         print("*",end = " ")
+# # # # # #     print()
+# # # # # # *
+# # # # # # * * 
+# # # # # # * * *
+# # # # # # * * * *
+# # # # # # n = int(input())
+# # # # # # for i in range(n+1):
+# # # # # #     for j in range(i):
+# # # # # #         print("*",end = " ")
+# # # # # #     print()
+# # # # # # * * * *
+# # # # # # * * *
+# # # # # # * * 
+# # # # # # *
+# # # # # n = int(input())
+# # # # # for i in range(n,0,-1):
+# # # # #     for j in range(i):
+# # # # #         print("*", end = " ")
+# # # # #     print()
+# # # # output is:
+# # # #     1
+# # # #     2 3
+# # # #     4 5 6
+# # # #     7 8 9 10
+# # # k = 1
+# # # n = int(input())
+# # # for i in range(1,n+1):
+# # #     for j in range(i):
+# # #         print(k,end = " ")
+# # #         k += 1
+# # #     print()
+# # n = int(input())
+# # for i in range(n):
+# #     for j in range(i+1):
+# #         print(chr(65+j),end = " ")
+# #     print()
+# n = int(input())
+# k = 65
+# for i in range(n):
+#     for j in range(i+1):
+#         print(chr(k),end = " ")
+#         k += 1
+#     print()
---
+++ IV sem/python/Practice/M02_Logic_Building_&Pattern/S05/PS06_Intermediante_patterns.py
@@ -0,0 +1,6 @@
+li = [1,2,3,4,5]
+#output = [1,4,9,16,25]
+res = []
+for i in li:
+    res.append(i**2)
+print(res)
