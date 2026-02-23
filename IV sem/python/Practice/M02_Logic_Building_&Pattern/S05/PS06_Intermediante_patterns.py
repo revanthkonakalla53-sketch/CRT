faa97911fd5762321c3b28c2a5acf009408f7b6c
revanthkonakalla53-sketch <revanthkonakalla53@gmail.com>
2026-02-23 10:33:59 +0530

    chnaged

--- IV sem/python/Practice/M02_Logic_Building_&Pattern/S05/PS06_Intermediante_patterns.py
+++ IV sem/python/Practice/M02_Logic_Building_&Pattern/S05/PS06_Intermediante_patterns.py
@@ -1,6 +1,18 @@
+''''
 li = [1,2,3,4,5]
 #output = [1,4,9,16,25]
 res = []
 for i in li:
     res.append(i**2)
 print(res)
+this is anoother 
+li = ['a','b','c',]
+res = ""
+for ch in li:
+    res = res + ch + " "
+print(res)
+print(" ".join(li))
+'''
+n = int(input())
+for i in range(1,n+1):
+    print(" "*(n-i) + "* "*i)
