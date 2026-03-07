0850ac18adc4d2c496b3233dae79ced9ddacd12d
revanthkonakalla53-sketch <revanthkonakalla53@gmail.com>
2026-03-07 21:18:36 +0530

    completed 2 assingments mam

---
+++ IV sem/python/Assignments/M02_Logic_Building_and_Patterns/AST01/task.py
@@ -0,0 +1,13 @@
+def count_digits(n: int) -> int:
+    if n == 0:
+        return 1
+    n = abs(n)
+    count = 0
+    while n > 0:
+        count += 1
+        n //= 10
+    return count
+
+if __name__ == "__main__":
+    n = int(input())
+    print(count_digits(n))
